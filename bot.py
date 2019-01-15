f338857d7484c440e1f76665dcc37dcef21efa29
brulite <35784437+brulite@users.noreply.github.com>
2019-01-15 02:50:11 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,13 +13,27 @@ from components import inlinequeries, taghints
 from const import (ENCLOSING_REPLACEMENT_CHARACTER, GITHUB_PATTERN, OFFTOPIC_CHAT_ID, OFFTOPIC_RULES,
                    OFFTOPIC_USERNAME, ONTOPIC_RULES, ONTOPIC_USERNAME)
 from util import get_reply_id, reply_or_edit, get_text_not_in_entities, github_issues
-
+import cherrypy
 if os.environ.get('ROOLSBOT_DEBUG'):
     logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                         level=logging.DEBUG)
 else:
     logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                         level=logging.INFO)
+from urllib.parse import quote, unquote
+
+class SimpleWebsite(object):
+    @cherrypy.expose
+    def index(self):
+        if 'content-length' in cherrypy.request.headers and \
+                'content-type' in cherrypy.request.headers and \
+                cherrypy.request.headers['content-type'] == 'application/json':
+            length = int(cherrypy.request.headers['content-length'])
+            json_string = cherrypy.request.body.read(length).decode("utf-8")
+            return """<H1>Welcome to Book bot!</H1>"""
+        else:
+            raise cherrypy.HTTPError(403)
+        
 
 logger = logging.getLogger(__name__)
 
@@ -250,8 +264,20 @@ def main():
 
     inlinequeries.register(dispatcher)
     dispatcher.add_error_handler(error)
+    TOKEN = config['KEYS']['bot_api']
 
-    updater.start_polling()
+    try:
+      updater.bot.setWebhook("https://yuirusbot.herokuapp.com/{}".format(TOKEN))
+    except:
+      raise RuntimeError("Failed to set the webhook")
+      pass
+    cherrypy.config.update({'server.socket_host': '127.0.0.1', })
+    cherrypy.config.update({'server.socket_port': int(3000), })
+    cherrypy.tree.mount(SimpleWebsite(), "/")
+    cherrypy.tree.mount("https://yuirusbot.herokuapp.com",
+                        "/{}".format(TOKEN),
+                        {'/': {'request.dispatch': cherrypy.dispatch.MethodDispatcher()}})
+    cherrypy.quickstart(SimpleWebsite(), "https://yuirusbot.herokuapp.com/{}".format(TOKEN), {'/': {}})
     logger.info('Listening...')
 
     try:
